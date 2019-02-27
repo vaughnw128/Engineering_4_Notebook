5c0e8cbd3498af02622064073c6803bcb912fb24
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2019-02-27 10:28:00 -0500

    Finished GPIO interactive website, complete with advanced graphics.

--- Python/Flask/GPIO/app.py
+++ Python/Flask/GPIO/app.py
@@ -6,17 +6,30 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
 
 app = Flask(__name__)
-@app.route("/", methods=["GET", "POST"])
 
+msg = "null"
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-	if request.method == "POST":
-		GPIO.output(17,GPIO.HIGH)
-		msg = request.form.get("submitBtn")
+	global msg
+	if request.method == 'POST':
+		if request.form.get('greensubmit.x'):
+			GPIO.output(17, GPIO.HIGH)
+			GPIO.output(27, GPIO.LOW)
+			msg = "GREENLEDHIGH"
+		if  request.form.get('bluesubmit.x'):
+			GPIO.output(17, GPIO.LOW)
+			GPIO.output(27, GPIO.HIGH)
+			msg = "BLUELEDHIGH"
+	elif request.method == 'GET':
+		GPIO.output(17, GPIO.LOW)
+		GPIO.output(27, GPIO.LOW)
+		msg = "PAGE RELOADED"
 	else:
-		GPIO.output(17,GPIO.LOW)
-		msg = "No click yet."
+		msg = "null"
 	return render_template("index.html", msg=msg)
 
 if __name__ == "__main__":
